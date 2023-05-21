1e6ce467da453b595dde102ba6a41a3ec9c0f988
franc <franceg.mier@gmail.com>
2023-05-22 00:01:46 +0800

    splash_screen

--- app/src/main/java/com/example/maven/MainActivity.java
+++ app/src/main/java/com/example/maven/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.maven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private static final long SPLASH_SCREEN_DELAY = 5000; // 5 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity after the delay
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Prevents the user from going back to the splash screen
+            }
+        }, SPLASH_SCREEN_DELAY);
     }
+
 }
